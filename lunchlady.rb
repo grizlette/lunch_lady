8e59543c92c30460ef161a46aa16377846e41b9b
Rosemary Thomas <rsm.thomas@gmail.com>
2015-05-28 16:25:51 -0600

    added menu hashes

--- lunchlady.rb
+++ lunchlady.rb
@@ -1,7 +1,18 @@
 class LunchLady
 
   def create_menu
+    main_dishes = {
+      1 => "chicken", 
+      2 => "fish", 
+      3 => "steak"
+      4 => "tofu"}
 
+    side_dishes = {
+      1 => "mashed potatoes"
+      2 => "asparagus"
+      3 => "salad"
+      4 => "soup"
+    }
   end
 
 end
